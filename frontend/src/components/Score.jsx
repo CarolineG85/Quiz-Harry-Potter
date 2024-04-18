33e9772edfbc071d5d10a535bbb35a590a3c74a4
CarolineG85 <caroline.gallix85@gmail.com>
2024-04-18 11:58:37 +0200

    making the score component

--- frontend/src/components/Score.jsx
+++ frontend/src/components/Score.jsx
@@ -1,10 +1,25 @@
 function Score() {
   return (
-    <div>
-      <h1>Voici ton résultat</h1>
+    <div className="score-container">
+      <div className="score-result">
+        <div className="result">
+          <h1> 🪄 Voici ton résultat 🪄</h1>
+        </div>
+        <div className="result-image">
+          <img src="/triwizard-tournament.gif" alt="Coupe des 3 sorciers" />
+        </div>
+        <div className="score">
+          <p>Tu as obtenu un score de sur </p>
+        </div>
+        <div className="restart">
+          <button type="button" className="restart-but">
+            Rejouer
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default Score;
-// TODO add a restart button
+// TODO add a restart button with a setTimeout in a useEffect?
